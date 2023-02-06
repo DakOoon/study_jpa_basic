36d5fc557607c5da966f00a3d56a5616fb013036
dakooon <wlaudwn007@gmail.com>
2023-02-07 01:59:04 +0900

    dev entity mapping 1

--- src/main/java/jpabasic/Member.java
+++ src/main/java/jpabasic/Member.java
@@ -2,12 +2,48 @@ package jpabasic;
 
 import jakarta.persistence.*;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+
 @Entity
+@Table(name= "MEMBER",
+        uniqueConstraints = { @UniqueConstraint(name = "UniqueNumberAndStatus",
+                columnNames = { "personNumber", "isActive" })})
+@SequenceGenerator(
+        name = "MEMBER_SEQ_GENERATOR",
+        sequenceName = "MEMBER_SEQ",
+        initialValue = 1,
+        allocationSize = 50)
 public class Member {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,
+            generator = "MEMBER_SEQ_GENERATOR")
     private Long id;
+
+    @Column(length = 10, updatable = false)
     private String name;
+
+    private Integer age;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+    private LocalDate noNeedTemporalDate;
+    private LocalDateTime noNeedTemporalDateTime;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+
+    @Lob
+    private String description;
+
+    @Transient
+    private int temp;
 
     public Long getId() {
         return id;
