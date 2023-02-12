cc9934f34dbb75967aaa01c348e76b48260a1a0d
dakooon <wlaudwn007@gmail.com>
2023-02-12 19:40:21 +0900

    dev various elation mapping type

---
+++ src/main/java/jpabasic/domain/Delivery.java
@@ -0,0 +1,20 @@
+package jpabasic.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Delivery {
+
+    @Id @GeneratedValue
+    @Column(name = "delivery_id")
+    private Long id;
+
+    private String city;
+    private String street;
+    private String zipcode;
+
+    private DeliveryStatus status;
+
+    @OneToOne(mappedBy = "delivery")
+    private Order order;
+}
---
+++ src/main/java/jpabasic/domain/DeliveryStatus.java
@@ -0,0 +1,4 @@
+package jpabasic.domain;
+
+public enum DeliveryStatus {
+}
